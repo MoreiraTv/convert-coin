a860ebbe70398ad841d380f0561e1b6dc8a65ee0
Matheus Moreira <matheusmoreira08@gmail.com>
2020-05-19 00:45:42 -0300

    ajust server 4

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-import Html from './public/index.html';
-import js from './src/index.js';
-
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8080, function () {
     console.log('Servidor lindão rodando!');
 });
 
-Html();
-js();
